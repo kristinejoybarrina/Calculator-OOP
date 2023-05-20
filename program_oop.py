b528e23c25b78c5e8f03e14feb5ad85c98c29267
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-05-20 16:19:44 +0800

    Pseudocode

---
+++ program_oop.py
@@ -0,0 +1,10 @@
+# Kristine Joy Barrina # BSCPE 1-5
+# Creating a calculator in an OOP way.
+
+# Pseudocode
+
+# ask user operation to be used
+# ask user first input
+# ask user second input
+# apply the operation chosen
+# display output
