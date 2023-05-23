f3d7456971d8d559d1339e4a5bcd2e795327562c
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-05-23 20:08:01 +0800

    Code not working huhu. Trying to fix and revise it.

--- program_oop.py
+++ program_oop.py
@@ -1,10 +1,17 @@
 # Kristine Joy Barrina # BSCPE 1-5
 # Creating a calculator in an OOP way.
 
+from operations import Operations
+from design_tkinter import Design
+
 # Pseudocode
 
 # ask user operation to be used
+Operations.operation_button()
+
 # ask user first input
+
+
 # ask user second input
 # apply the operation chosen
 # display output
