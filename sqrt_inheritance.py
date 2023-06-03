385eb7bfe79bf13384e025b597cb3b45ea8c3dbf
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-06-03 20:11:06 +0800

    Create a working function for sqrt method

--- sqrt_inheritance.py
+++ sqrt_inheritance.py
@@ -1,4 +1,10 @@
+# Create an inheritance from calculator.py
+
+import math
 from calculator import Calculator
 
 class Calculator (Calculator):
-    pass
+    
+    def square_root(self, num1, num2):
+        square_root = math.sqrt (num1, num2)
+        return square_root
