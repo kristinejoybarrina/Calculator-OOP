2d8ddab50ee0c53c028d333f117a85ef407e2d71
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-06-03 21:02:42 +0800

    Create an inheritance for square root's design

---
+++ designed_calc_inheritance.py
@@ -0,0 +1,8 @@
+# Create a inheritance for square root's design
+
+from designed_calculator import Design
+from sqrt_inheritance import Calculator
+
+# Create a class for scientific calculator
+class SqrtCalc(Design, Calculator):
+    pass
